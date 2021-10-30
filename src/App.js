86b0c1ac615ce442d0f4b90ccca46ecffeacf326
Gabriel Simard <gabriel.simard.4@gmail.com>
2021-10-30 15:14:57 -0400

    Home page with components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import AppLogo from './components/AppLogo';
+import Body from './components/Body';
+import DocumentationLink from './components/DocumentationLink';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AppLogo logo={logo}/>
+        
+        <Body/>
+
+        <DocumentationLink/>
       </header>
     </div>
   );
